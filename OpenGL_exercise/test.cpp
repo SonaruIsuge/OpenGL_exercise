cc7578405f513d3f18a508199608fcb71658249b
Sonaru <bread511206@gmail.com>
2024-01-20 19:20:50 +0800

    [Add] Set opengl environment

---
+++ OpenGL_exercise/test.cpp
@@ -0,0 +1,74 @@
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+using namespace std;
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+
+int main()
+{
+	// Init GLFW
+	glfwInit();
+
+	// Set all requirements for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create GLFWwindow object that can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(800, 600, "TestOpenGL", nullptr, nullptr);
+	if (window == nullptr) {
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (glewInit() != GLEW_OK) {
+		cout << "Failed to initialize GLEW" << endl;
+		return -1;
+	}
+
+	// Define viewport dimensions
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	// Game loop
+	while (!glfwWindowShouldClose(window)) {
+		// Check events (key pressed, mouse move ect.) and call corresponding respose functions
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap the screen buffers (Double buffers)
+		glfwSwapBuffers(window);
+	}
+
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+	return 0;
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
